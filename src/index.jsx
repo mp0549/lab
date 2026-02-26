acc86552f0eab7b96eddf9a3aa7488eb02251aae
mp0549 <maanan@gmail.com>
2026-02-26 13:28:10 -0600

    fix hash 3

--- src/index.jsx
+++ src/index.jsx
@@ -7,7 +7,6 @@ const container = document.getElementById('root');
 if (container) {
   const root = createRoot(container);
   root.render(
-    // <HashRouter basename="/lab">
     <HashRouter>
       <App />
     </HashRouter>
